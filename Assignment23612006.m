0844e468fe7f3335e31112e86819286983b4d9ef
Talhaiitr <140053417+Talhaiitr@users.noreply.github.com>
2023-08-07 11:27:09 +0530

    Add files via upload
    Assignment 1 class 1

---
+++ Assignment23612006.m
@@ -0,0 +1,16 @@
+a = 21;
+b = 14;
+c = 13
+d = 17
+e = input ( "write the valueof e\n" );
+e = input ( "write the value of f\n");
+e = input ( "write the value of g\n");
+e = input ( "write the value of h\n");
+w = a + e;
+x = b - e;
+y = c * e;
+z = d / e;
+fprintf( "%d + %d = %d\n", a,e,w );
+fprintf ( "%d - %d = %d\n", b,e,x );
+fprintf ( "%d * %d = %d\n", c,e,y );
+fprintf ( "%d / %d = %d\n", d,e,z );
