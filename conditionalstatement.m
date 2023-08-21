5cf5a5c97bd21290298adb9483b4e25cdb07b4d8
Talhaiitr <140053417+Talhaiitr@users.noreply.github.com>
2023-08-21 23:45:31 +0530

    Add files via upload

---
+++ conditionalstatement.m
@@ -0,0 +1,88 @@
+a = 5 
+b = 3 
+a == b 
+b = 4
+a >= b
+b = 4 
+a >= b
+b = 5
+a >= b
+b = 6
+a >= b
+a > b
+a = 5 
+b = 4 
+if ( a > b ) 
+disp ( " a is larger than b " ) 
+end
+c = 5; % in km 
+d = 6; % in km 
+if ( c < d ) 
+disp = ( " c is smaller than " ) 
+end 
+e = 4 
+a = 5 
+b = 4 
+if ( a > b ) 
+disp ( " a is larger than b " );
+end
+c = 5; % in km 
+d = 6; % in km 
+if ( c < d ) 
+disp = ( " c is smaller than " ) 
+end 
+e = 4 
+a = 3
+a = 5 
+b = 4 
+if ( a > b ) 
+disp ( " a is larger than b " ) 
+end
+c = 5; % in km 
+d = 6; % in km 
+if ( c < d ) 
+disp = ( " c is smaller than " ) 
+end 
+e = 4 
+b = 4 
+a > b 
+if a > b 
+    c = a*b; 
+    fprintf('product = %d \n' ,c) 
+elseif a==b;
+    c = a*a*al;
+     fprintf( 'val %d' ,c)
+elseif a==4
+    c = a*a*a;
+    fprintf( 'val is 4')
+else
+    c = a/b;
+    fprintf('division = %d /n',c)
+end
+if (( a > b)&&( a==4 ))
+disp ( " a is larger than b " );
+end
+c = 5; % in km 
+d = 6; % in km 
+if ( c < d ) 
+disp = ( " c is smaller than " ) 
+end 
+e = 4 
+a = 3
+b = 4 
+a > b 
+if a > b 
+    c = a*b; 
+    fprintf('product = %d \n' ,c) 
+elseif a==b;
+    c = a*a*al;
+     fprintf( 'val %d' ,c)
+elseif a==4
+    c = a*a*a;
+    fprintf( 'val is 4')
+else
+    c = a/b;
+    fprintf('division = %d /n',c)
+end
+
+    
